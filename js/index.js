50918f95b36e183085c6138f434c561c67fa9424
Yusuf Nafey <yusufnafey@yahoo.com>
2019-09-09 17:06:47 -0700

    finishes mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+const mainTitle = document.querySelector(".logo-heading");
+mainTitle.addEventListener("copy", e => {
+  mainTitle.style.display = "none";
+});
+
+window.addEventListener("keydown", e => {
+  const welcomeTitle = document.querySelector(".home .intro h2");
+  welcomeTitle.textContent = "You Pressed A Button!";
+});
+
+const busImage = document.querySelector(".home .intro img");
+busImage.addEventListener("contextmenu", e => {
+  busImage.src =
+    "https://images.unsplash.com/photo-1517259059569-1501714ec1ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80";
+});
+
+const firstParagraph = document.querySelector("p");
+firstParagraph.addEventListener("click", e => {
+  firstParagraph.style.color = "red";
+});
+
+const header = document.querySelector(".container .intro");
+header.addEventListener("dblclick", e => {
+  header.style.background = "green";
+});
+
+const riverImage = document.querySelector(".inverse-content .img-content img");
+riverImage.addEventListener("mouseover", e => {
+  e.currentTarget.style.display = "none";
+});
